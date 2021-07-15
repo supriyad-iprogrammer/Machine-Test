1c6ff6c3af89f7dce1d80c0cb71b8247f8f0dd6a
supriyad-iprogrammer <supriyad@iprogrammer.co>
2021-07-15 18:50:57 +0530

    create vastu score component

--- src/app/core/constant/serverurl.constant.ts
+++ src/app/core/constant/serverurl.constant.ts
@@ -4,7 +4,10 @@ export class ServerUrl {
 
  public static MAIN = environment.baseUrl
  public static API_ENDPOINT_GET_ROOMLIST: string = '/vastu/getRoomList';
-
+ public static API_GET_ROOMDETAILS_DIRECTION:string='/vastuCheck/getDirectionDetails'
+//  public static API_ENDPOINT_GET_ROOMDETAILS: string = 'getRoomDetails';
+//  public static API_ENDPOINT_GET_DIRECATION_DETAILS: string = 'getDirectionDetails';
+//  public static API_ENDPOINT_VAASTU_SCORE: string = 'getVastuScore';
 
 
 }
