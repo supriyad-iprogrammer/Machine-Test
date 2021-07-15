5d0a72228d37c04d76cd2f1e01733b9263ede2ca
supriyad-iprogrammer <supriyad@iprogrammer.co>
2021-07-15 13:55:06 +0530

    Request Get Api

---
+++ src/app/core/constant/serverurl.constant.ts
@@ -0,0 +1,10 @@
+import { environment } from "./../../../environments/environment";
+
+export class ServerUrl {
+
+ public static MAIN = environment.baseUrl
+ public static API_ENDPOINT_GET_ROOMLIST: string = '/vastu/getRoomList';
+
+
+
+}
